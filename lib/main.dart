41ce90a62f7bc930b486c2671b6fabd867e866f3
sehan22 <sehanranaweera2002@gmail.com>
2023-12-12 20:26:22 +0530

    AppBar Updated.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,54 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
 
-main() {
-  runApp(MyApp());
-}
+void main() => runApp(
+    // DevicePreview(
+    //   enabled: !kReleaseMode,
+    //   builder: (context) => MyApp(), // Wrap your app
+    // ),
+    MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text("Hello"),
+    return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: MyHomePage(),
     );
   }
+}
 
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: (){},
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: (){},
+          ),
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: (){},
+          ),
+        ],
+      ),
+    );
+  }
 }
